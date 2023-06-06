a40ffbb0b0f05aac5d6cbc24fec09c7b5b530c91
Xin Yu Zhenge <xy.zheng@live.com>
2023-06-06 18:01:20 +1000

    got chevron arrows working with setting date

--- src/components/DateNavigation.jsx
+++ src/components/DateNavigation.jsx
@@ -15,24 +15,33 @@ const DateNavigation = () => {
     setShowCalendar(true)
   }
 
+  const handleLeftArrow = () => {
+    setDate(new Date(date.getTime() - 86400000))
+  }
+
+  const handleRightArrow = () => {
+    setDate(new Date(date.getTime() + 86400000))
+    console.log(new Date(date.getTime() + 86400000))
+  }
+
   return (
     <Grid container direction="column">
       <Grid container direction="row">
         <Grid item> 
-          <Button> <ArrowBackIosIcon/> </Button>
+          <Button onClick={handleLeftArrow}> <ArrowBackIosIcon/> </Button>
         </Grid>
         <Grid item> 
           <Button onClick={handleShowCalendar}> 
           {
             date.toISOString().split('T')[0] === new Date().toISOString().split('T')[0] ? "TODAY" :
-            date.toISOString().split('T')[0] === new Date(new Date().setDate(new Date().getDate() - 1)).toISOString().split('T')[0] ? "YESTERDAY" :
-            date.toISOString().split('T')[0] === new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split('T')[0] ? "TOMORROW" :
+            date.toISOString().split('T')[0] === new Date(Date.now() - 86400000).toISOString().split('T')[0] ? "YESTERDAY" :
+            date.toISOString().split('T')[0] === new Date(Date.now() + 86400000).toISOString().split('T')[0] ? "TOMORROW" :
             date.toDateString()
           }
           </Button>
         </Grid>
         <Grid item> 
-          <Button> <ArrowForwardIosIcon/> </Button>
+          <Button onClick = {handleRightArrow}> <ArrowForwardIosIcon/> </Button>
         </Grid>
       </Grid>
       {showCalendar && 
