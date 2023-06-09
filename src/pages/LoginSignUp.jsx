5ecbea3d0e00ce76328493d27c488ef59ee5785b
Xin Yu Zhenge <xy.zheng@live.com>
2023-06-10 07:27:38 +1000

    got rid of front page image

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,7 +1,7 @@
 import Login from "../components/Login"
 import SignUp  from"../components/SignUp"
 import { useState } from "react"
-import { Grid } from "@mui/material"
+import { Grid, Box, Typography } from "@mui/material"
 
 const LoginSignUp = () => {
   const [hasAccount, setHasAccount] = useState(true)
@@ -10,18 +10,19 @@ const LoginSignUp = () => {
   }
 
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center" 
-      style={{ height: '100vh'}} sx={{backgroundColor: 'rgb(234, 233, 239)'}}>
-        <Grid item xs={6}>
-        </Grid>
-        <Grid item xs={3} sx={{border: '1px solid grey', borderRadius: 2, p: 10, backgroundColor: 'white'}}>
-          {hasAccount ? 
-          <Login toggleForm = {toggleForm}/> 
-          : 
-          <SignUp toggleForm = {toggleForm}/>
-          }
-        </Grid>
+  <>
+    <Grid textAlign="center" sx={{backgroundColor: 'rgb(234, 233, 239)', paddingTop: 4, paddingBottom: 0}}>
+      <Typography variant="h1"> Macro Health Tracking</Typography>
+    </Grid>
+    <Grid container direction="row" justifyContent="center" alignItems="center"
+      style={{ height: '88vh'}} sx={{backgroundColor: 'rgb(234, 233, 239)'}}>
+      <Grid sx={{border: '1px solid grey', borderRadius: 2, p: 10, backgroundColor: 'white', marginBottom: 4}}>
+        {hasAccount ? 
+        <Login toggleForm = {toggleForm}/> : <SignUp toggleForm = {toggleForm}/>
+        }
       </Grid>
+    </Grid>
+  </>
   )
 }
 
