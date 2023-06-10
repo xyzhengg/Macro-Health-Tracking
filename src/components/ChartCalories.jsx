77da8e235e789e2eeebe783fa3152a5b81aae974
Xin Yu Zhenge <xy.zheng@live.com>
2023-06-10 12:52:38 +1000

    fixed data grabbing for Calorie Charts - getting the correct data for the correct dates

--- src/components/ChartCalories.jsx
+++ src/components/ChartCalories.jsx
@@ -18,11 +18,12 @@ export default function ChartCalories () {
       dateArray.unshift({
       name: date.toDateString(),
       date: date.toISOString().split('T')[0],
-      calories: 0
+      caloriesIn: 0
     })
     date = new Date(date.getTime() - 86400000)
     }
     setCalorieData([...dateArray])
+    // console.log([...dateArray])
     // console.log(dateArray)
   }, [])
 
@@ -37,30 +38,30 @@ export default function ChartCalories () {
               .select('created_at, calories')
               .eq('user_id', user)
               .lte('created_at', `${date.toISOString().split('T')[0]} 23:59:59`)
-              .gte('created_at', `${new Date(date.getTime() - 604800000).toISOString().split('T')[0]} 00:00:00`)
+              .gte('created_at', `${new Date(date.getTime() - 518400000).toISOString().split('T')[0]} 00:00:00`)
               .order('created_at', { ascending: true }),
             supabase
               .from('calories_out')
               .select('created_at, calories')
               .eq('user_id', user)
               .lte('created_at', `${date.toISOString().split('T')[0]} 23:59:59`)
-              .gte('created_at', `${new Date(date.getTime() - 604800000).toISOString().split('T')[0]} 00:00:00`)
+              .gte('created_at', `${new Date(date.getTime() - 518400000).toISOString().split('T')[0]} 00:00:00`)
               .order('created_at', { ascending: true })
           ])
-  
+          // console.log(`${date.toISOString().split('T')[0]} 23:59:59`)
+          // console.log(`${new Date(date.getTime() - 518400000).toISOString().split('T')[0]} 00:00:00`)
           if (diaryData.error || caloriesOutData.error) {
             console.log(diaryData.error || caloriesOutData.error)
           } else {
-            // console.log(diaryData.data)
             // console.log(caloriesOutData.data)
-  
+            console.log(diaryData.data)
             const updatedCalorieData = []
             for (let item of calorieData) {
               const diaryMatches = diaryData.data.filter(entry => {
-                const entryDate = new Date(entry.created_at).toISOString().split('T')[0]
+                const entryDate = entry.created_at.split('T')[0]
                 return entryDate === item.date
               })
-              let totalCalories = Math.round(item.calories)
+              let totalCalories = Math.round(item.caloriesIn)
               for (let i = 0; i < diaryMatches.length; i++) {
                 totalCalories += diaryMatches[i].calories
               }
@@ -72,7 +73,7 @@ export default function ChartCalories () {
             }
   
             setCalorieData(updatedCalorieData)
-            // console.log(updatedCalorieData)
+            console.log(updatedCalorieData)
           }
         }
       } catch (err) {
