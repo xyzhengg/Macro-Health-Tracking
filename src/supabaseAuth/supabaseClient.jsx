aa156a54ed63076fa20daa5fb92859f3f5e0e38a
Xin Yu Zhenge <xy.zheng@live.com>
2023-11-14 22:02:10 +1100

    edit suapbase client

--- src/supabaseAuth/supabaseClient.jsx
+++ src/supabaseAuth/supabaseClient.jsx
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.VITE_SUPABASE_URL
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
